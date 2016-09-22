4474ecd504f5eddc63bf068a6553750ae3ac872e
Ivan <madritistaaa@gmail.com>
2016-09-22 20:22:32 +0300

    Create base class

--- public/scripts/models/base-classes.js
+++ public/scripts/models/base-classes.js
@@ -1,5 +1,34 @@
+import { idGenerator } from '../utils/data-generator';
+
 class Item {
     constructor(name, price, category) {
+        this.id = idGenerator.next().value;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
 
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        this._name = value;
+    }
+
+    get price() {
+        return this._price;
+    }
+
+    set price(value) {
+        this._price = value;
+    }
+
+    get category() {
+        return this._category;
+    }
+
+    set category(value) {
+        this._category = value;
     }
 }
