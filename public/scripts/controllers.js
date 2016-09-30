7e340d050b2194c4747bac517a78e703052b0ce4
Ivan <madritistaaa@gmail.com>
2016-10-01 00:21:04 +0300

    Fix

--- public/scripts/controllers.js
+++ public/scripts/controllers.js
@@ -1,30 +1,38 @@
 let controllers = {
     home: () => {
-
+        console.log(5);
     },
     products: () => {
 
+        console.log(5);
     },
     books: () => {
 
+        console.log(5);
     },
     magazines: () => {
 
+        console.log(5);
     },
     comics: () => {
 
+        console.log(5);
     },
     cds: () => {
 
+        console.log(5);
     },
     dvds: () => {
 
+        console.log(5);
     },
     contact: () => {
 
+        console.log(5);
     },
     login: () => {
 
+        console.log(5);
     }
 };
 
