f3688476b9b38551026d30ecbcac702db648b287
Ivan <madritistaaa@gmail.com>
2016-09-30 22:45:45 +0300

    Server is running

---
+++ app.js
@@ -0,0 +1,30 @@
+/* globals require console */
+"use strict";
+
+const express = require("express"),
+    bodyParser = require("body-parser"),
+    lowdb = require("lowdb"),
+    cors = require("cors");
+
+let db = lowdb("./data/data.json");
+db._.mixin(require("underscore-db"));
+
+let app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(express.static("public"));
+
+// require("./utils/authorize-user")(app, db);
+
+let usersController = require("./controllers/users-controller")(db);
+app.get("/api/users", usersController.get);
+app.post("/api/users", usersController.post);
+app.put("/api/auth", usersController.put);
+
+let productsController = require("./controllers/products-controller")(db);
+app.get("/api/products", productsController.get);
+// app.post("/api/products", productsController.post);
+
+let port = 3000;
+app.listen(port, () => console.log(`Server is running at http://localhost:${port}`));
