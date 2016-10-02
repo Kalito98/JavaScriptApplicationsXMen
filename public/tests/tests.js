215d0d8173acea8054723b51c0d70165916abfe8
Ivan <madritistaaa@gmail.com>
2016-10-02 19:15:44 +0300

    Finish tests

--- public/tests/tests.js
+++ public/tests/tests.js
@@ -331,6 +331,163 @@ describe('Tests', function () {
                 .then(done, done);
         });
     });
+
+    describe('Login tests', function () {
+        const user = {
+            username: 'pesho',
+            passHash: 'peshoPass'
+        };
+
+        const AUTH_KEY = "123456789";
+
+        beforeEach(function () {
+            sinon.stub(requester, 'putJSON')
+                .returns(new Promise((resolve, reject) => {
+                    resolve({
+                        result: {
+                            username: user.username,
+                            authKey: AUTH_KEY
+                        }
+                    });
+                }));
+        });
+
+        afterEach(function () {
+            requester.putJSON.restore();
+            localStorage.clear();
+        });
+
+        it('expect putJSON to be called once', function (done) {
+            dataService.login(user)
+                .then(() => {
+                    expect(requester.putJSON.calledOnce).to.be.true;
+                })
+                .then(done, done);
+        });
+
+        it('expect putJSON to make correct call', function (done) {
+            dataService.login(user)
+                .then(() => {
+                    let actual = requester.putJSON
+                        .firstCall
+                        .args;
+
+                    expect(actual.length).to.equal(2);
+                    expect(actual[0]).to.equal('/api/auth');
+                    expect(actual[1]).to.eql(user);
+                })
+                .then(done, done);
+        });
+
+        it('expect login to put username in local storage', function (done) {
+            dataService.login(user)
+                .then(() => {
+                    expect(localStorage.getItem('username')).to.equal(user.username);
+                })
+                .then(done, done);
+        });
+
+        it('expect login to put auth key in local storage', function (done) {
+            dataService.login(user)
+                .then(() => {
+                    expect(localStorage.getItem('authKey')).to.equal(AUTH_KEY);
+                })
+                .then(done, done);
+        });
+    });
+
+    describe('Logout tests', function () {
+        const user = {
+            username: 'pesho',
+            passHash: 'peshoPass'
+        };
+
+        const AUTH_KEY = "123456789";
+
+        beforeEach(function () {
+            sinon.stub(requester, 'putJSON')
+                .returns(new Promise((resolve, reject) => {
+                    resolve({
+                        result: {
+                            username: user.username,
+                            authKey: AUTH_KEY
+                        }
+                    });
+                }));
+        });
+
+        afterEach(function () {
+            requester.putJSON.restore();
+            localStorage.clear();
+        });
+
+        it('expect local storage to not have key username', function (done) {
+            dataService.login(user)
+                .then(() => {
+                    dataService.logout();
+                })
+                .then(() => {
+                    expect(localStorage.getItem('username')).to.be.null;
+                })
+                .then(done, done);
+        });
+
+        it('expect local storage to not have key auth key', function (done) {
+            dataService.login(user)
+                .then(() => {
+                    dataService.logout();
+                })
+                .then(() => {
+                    expect(localStorage.getItem('authKey')).to.be.null;
+                })
+                .then(done, done);
+        });
+    });
+
+    describe('Is logged in tests', function () {
+        const user = {
+            username: 'pesho',
+            passHash: 'peshoPass'
+        };
+
+        const AUTH_KEY = "123456789";
+
+        beforeEach(function () {
+            sinon.stub(requester, 'putJSON')
+                .returns(new Promise((resolve, reject) => {
+                    resolve({
+                        result: {
+                            username: user.username,
+                            authKey: AUTH_KEY
+                        }
+                    });
+                }));
+        });
+
+        afterEach(function () {
+            requester.putJSON.restore();
+            localStorage.clear();
+        });
+
+        it('expect to return false when not logged in', function (done) {
+            dataService.isLoggedIn()
+                .then((res) => {
+                    expect(res).to.be.false;
+                })
+                .then(done, done);
+        });
+
+        it('expect to return true when logged in', function (done) {
+            dataService.login(user)
+                .then(() => {
+                    return dataService.isLoggedIn();
+                })
+                .then((res) => {
+                    expect(res).to.be.true;
+                })
+                .then(done, done);
+        });
+    });
 });
 
 mocha.run();
